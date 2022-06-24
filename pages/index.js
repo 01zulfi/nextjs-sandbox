c409a4a9c32869d025c9ee862ec716e30b224d2f
01zulfi <85733202+01zulfi@users.noreply.github.com>
2022-06-24 19:08:39 +0500

    Update bio

--- pages/index.js
+++ pages/index.js
@@ -21,7 +21,7 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Learning new things all the time</p>
+        <p>Learning new things all the time!</p>
         <p>
           (This is a sample website - you’ll be building a site like this on{" "}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
